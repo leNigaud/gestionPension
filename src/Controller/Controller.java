e273ee033ace2d7ebaecd04700016731efc733ea
R4NJI <njarasoatoavinarandrianarivo@gmail.com>
2023-06-27 10:18:45 +0200

    debut controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,43 @@
 package Controller;
 
+import Model.*;
+import View.*;
+
 public class Controller {
+    private Model myModel;
+    private View myView;
+
+    public Controller() {
+
+        myModel = new Model();
+        myView = new View();
+
+        //menu nouveau 
+                //recuperer les valeurs saisies par l'utilisateur dans la fenetre personne et les stocker dans la base de donnees
+                    
+
+                //recuperer les valeurs saisies par l'utilisateur dans la fenetre paiement et les stocker dans la base de donnees
+
+                //recuperer les valeurs saisies par l'utilisateur dans la fenetre tarif et les stocker dans la base de donnees
+        
+        
+        //menu personne
+
+
+        //menu paiement
+
+
+        //menu tarif 
+
+
+        //menu histogramme
+
+
+
+
+
+    }
+
     
+
 }
